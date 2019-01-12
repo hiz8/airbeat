44a4fd6631d1a65bdbd9f82bc48229bc9e663535
hirofumi_i <detroit69city.nau@gmail.com>
2019-01-12 19:29:56 +0900

    Remove unnecessary files and descriptions

--- actions/index.ts
+++ actions/index.ts
@@ -1,25 +1,8 @@
 export enum actionTypes {
-  ADD = 'ADD',
-  TICK = 'TICK',
   UPDATE_RUN_STATUS = 'UPDATE_RUN_STATUS',
 }
 
 // ACTIONS
-export const serverRenderClock = isServer => dispatch => {
-  return dispatch({ type: actionTypes.TICK, light: !isServer, ts: Date.now() });
-};
-
-export const startClock = () => dispatch => {
-  return setInterval(
-    () => dispatch({ type: actionTypes.TICK, light: true, ts: Date.now() }),
-    1000,
-  );
-};
-
-export const addCount = () => dispatch => {
-  return dispatch({ type: actionTypes.ADD });
-};
-
 export const updateRunStatus = () => dispatch => {
   return dispatch({ type: actionTypes.UPDATE_RUN_STATUS });
 };
--- reducers/index.ts
+++ reducers/index.ts
@@ -1,11 +1,7 @@
 import { combineReducers } from 'redux';
-import { reducer as tickReducer } from './tick';
-import { reducer as addReducer } from './add';
 import { reducer as updateRunStatusReducer } from './updateRunStatus';
 
 // REDUCERS
 export default combineReducers({
-  tick: tickReducer,
-  add: addReducer,
   updateRunStatus: updateRunStatusReducer,
 });
