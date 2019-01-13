62b64c570e7de910fde64719dd4c30adc40e45f6
hirofumi_i <detroit69city.nau@gmail.com>
2019-01-13 13:33:21 +0900

    Implement tempo manipulation with Range input

--- components/TempoController.tsx
+++ components/TempoController.tsx
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux';
 import { updateTempo } from '../actions';
 
 import { merge, fromEvent } from 'rxjs';
-import { map, mapTo, scan, filter } from 'rxjs/operators';
+import { map, mapTo, scan } from 'rxjs/operators';
 
 interface IProps {
   updateTempo: (value: number) => void;
@@ -13,27 +13,46 @@ interface IProps {
 }
 
 class TempoController extends Component<IProps> {
+  private readonly MAXIMUM_TEMPO = 208;
+  private readonly MINIMUM_TEMPO = 40;
+
   constructor(props) {
     super(props);
   }
 
   public componentDidMount() {
+    /**
+     * プラス・マイナスボタンのクリックイベントからストリームを生成
+     */
     const plusButton: HTMLElement = document.getElementById('plus');
-    const minusButton: HTMLElement = document.getElementById('minus');
-
     const plusStream = fromEvent(plusButton, 'click').pipe(mapTo(1));
+    const minusButton: HTMLElement = document.getElementById('minus');
     const minusStream = fromEvent(minusButton, 'click').pipe(mapTo(-1));
 
     const clickStream = merge(plusStream, minusStream);
 
-    merge(clickStream)
+    /**
+     * Range input の操作からストリームを生成
+     */
+    const rangeInput = document.getElementById('range');
+    const rangeInputStream = fromEvent(rangeInput, 'input').pipe(
+      map((val: any) => {
+        return val.target.value;
+      }),
+    );
+
+    /**
+     * ストリームをマージし、stateの更新へ
+     */
+    merge(clickStream, rangeInputStream)
       .pipe(
         scan((acc, curr) => {
           if (curr >= 2) {
             return parseInt(curr.toString(), 10);
           } else {
             const math = acc + curr;
-            if (40 > math || math > 208) {
+
+            if (this.MINIMUM_TEMPO > math || math > this.MAXIMUM_TEMPO) {
               return acc;
             } else {
               return math;
@@ -55,6 +74,15 @@ class TempoController extends Component<IProps> {
         <button type="button" id="minus">
           マイナス
         </button>
+
+        <input
+          id="range"
+          type="range"
+          min={this.MINIMUM_TEMPO}
+          max={this.MAXIMUM_TEMPO}
+          step="1"
+          value={this.props.tempo}
+        />
       </>
     );
   }
