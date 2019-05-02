cf186faaefb44dd59636e389a6b55f3e4d132189
hirofumi_i <detroit69city.nau@gmail.com>
2019-05-02 23:20:07 +0900

    Update workbox options

--- next.config.js
+++ next.config.js
@@ -3,6 +3,42 @@ const withOffline = require('next-offline');
 
 const nextConfig = {
   generateInDevMode: true,
+  workboxOpts: {
+    globPatterns: ['static/**/*'],
+    globDirectory: '.',
+    importWorkboxFrom: 'cdn',
+    skipWaiting: true,
+    clientsClaim: true,
+    runtimeCaching: [
+      {
+        urlPattern: '/',
+        handler: 'networkFirst',
+        options: {
+          cacheName: 'html-cache',
+        },
+      },
+      {
+        urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
+        handler: 'cacheFirst',
+        options: {
+          cacheName: 'image-cache',
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+      {
+        urlPattern: /.*\.(?:ttf|woff|woff2)/,
+        handler: 'cacheFirst',
+        options: {
+          cacheName: 'font-cache',
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
 };
 
 module.exports = withTypescript(withOffline(nextConfig));
