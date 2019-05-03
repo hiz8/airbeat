a725c01aaa982051c1d148e4140e0b1ddd123c1c
hirofumi_i <detroit69city.nau@gmail.com>
2019-05-03 13:49:39 +0900

    Remove unnecessary console.log

--- static/js/metronome.worker.js
+++ static/js/metronome.worker.js
@@ -2,16 +2,13 @@ var timerID = null;
 var interval = 100;
 
 self.onmessage = e => {
-  console.log('e is', e);
   if (e.data == 'start') {
-    console.log('starting');
     timerID = setInterval(() => {
       postMessage('tick');
     }, interval);
   } else if (e.data.interval) {
-    console.log('setting interval');
     interval = e.data.interval;
-    console.log('interval=' + interval);
+
     if (timerID) {
       clearInterval(timerID);
       timerID = setInterval(() => {
@@ -19,10 +16,7 @@ self.onmessage = e => {
       }, interval);
     }
   } else if (e.data == 'stop') {
-    console.log('stopping');
     clearInterval(timerID);
     timerID = null;
   }
 };
-
-postMessage('hi there');
