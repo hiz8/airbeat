2bc2992c3ade9da05b820832725c5fb8bd210fdf
hirofumi_i <detroit69city.nau@gmail.com>
2019-06-02 23:42:46 +0900

    Add polyfill of Web Animations API

--- lib/polyfills.ts
+++ lib/polyfills.ts
@@ -1,3 +1,7 @@
 // Pointer events
 // @see https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events
 import 'pepjs';
+
+// Web Animations API for Edge
+// @see https://github.com/web-animations/web-animations-js
+import 'web-animations-js/web-animations.min.js';
