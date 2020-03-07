c256c21af2f99586ade26d1186edba6711d75126
hirofumi_i <detroit69city.nau@gmail.com>
2020-03-07 11:38:46 +0900

    Update next-offline to v5

--- next.config.js
+++ next.config.js
@@ -21,22 +21,17 @@ const nextConfig = {
   },
   generateInDevMode: true,
   workboxOpts: {
-    globPatterns: ['static/**/*'],
-    globDirectory: '.',
-    importWorkboxFrom: 'cdn',
-    skipWaiting: true,
-    clientsClaim: true,
     runtimeCaching: [
       {
         urlPattern: '/',
-        handler: 'networkFirst',
+        handler: 'NetworkFirst',
         options: {
           cacheName: 'html-cache',
         },
       },
       {
         urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
-        handler: 'cacheFirst',
+        handler: 'CacheFirst',
         options: {
           cacheName: 'image-cache',
           cacheableResponse: {
@@ -46,7 +41,7 @@ const nextConfig = {
       },
       {
         urlPattern: /.*\.(?:ttf|woff|woff2)/,
-        handler: 'cacheFirst',
+        handler: 'CacheFirst',
         options: {
           cacheName: 'font-cache',
           cacheableResponse: {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Page from '../components/Page';
 
 export default () => {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('./service-worker.js')
-        .catch(err => console.error('Service worker registration failed', err));
-    } else {
-      console.log('Service worker not supported');
-    }
-  });
-
   return <Page />;
 };
