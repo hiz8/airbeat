c58564e13aae0cf69f6b40d79287368a09eb91bc
hirofumi_i <detroit69city.nau@gmail.com>
2022-01-08 04:00:34 +0900

    Fixed meta tag placement location

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,21 +37,6 @@ export default class MyDocument extends Document<IProps> {
     return (
       <Html lang="en">
         <Head>
-          <meta
-            name="description"
-            content="Offline supported metronome application"
-          />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, user-scalable=no, shrink-to-fit=no"
-          />
-          <link rel="canonical" href="https://airbeat.plyrs.app/" />
-          <link rel="manifest" href="/static/app.webmanifest" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="application-name" content="airbeat" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-          <meta name="apple-mobile-web-app-title" content="airbeat" />
           <link
             rel="icon"
             sizes="192x192"
